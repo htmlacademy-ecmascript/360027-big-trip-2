8c267ec3c690fc6f2d2702fa5d18d59b8d3cae22
maxim.ananyev <maksim.ve@gmail.com>
2026-05-06 18:00:33 +0300

    Добавит правки после ревью ДЗ module1_task3

--- src/presenter/board-presenter.js
+++ src/presenter/board-presenter.js
@@ -1,6 +1,6 @@
 import SortView from '../view/sort-view.js';
 import EventListView from '../view/event-list-view.js';
-import EditPointView from '../view/edit-point-view.js';
+import EditPointView from '../view/form-point-view.js';
 import PointView from '../view/point-view.js';
 import {render} from '../render.js';
 
@@ -17,7 +17,6 @@ export default class BoardPresenter {
     render(new SortView(), this.boardContainer);
     render(this.eventListComponent, this.boardContainer);
 
-    render(new PointView(), this.eventListComponent.getElement());
     render(new EditPointView(), this.eventListComponent.getElement());
 
     for (let i = 0; i < POINTS_COUNT; i++) {
