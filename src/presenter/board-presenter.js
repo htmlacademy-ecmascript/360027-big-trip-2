7939f54774729e4a7d78c8552d4a08c35ff02785
maxim.ananyev <maksim.ve@gmail.com>
2026-05-07 11:39:53 +0300

    Подключит модель

--- src/presenter/board-presenter.js
+++ src/presenter/board-presenter.js
@@ -4,22 +4,23 @@ import EditPointView from '../view/form-point-view.js';
 import PointView from '../view/point-view.js';
 import {render} from '../render.js';
 
-const POINTS_COUNT = 3;
-
 export default class BoardPresenter {
   eventListComponent = new EventListView();
 
-  constructor({boardContainer}) {
+  constructor({boardContainer, pointsModel}) {
     this.boardContainer = boardContainer;
+    this.pointsModel = pointsModel;
   }
 
   init() {
+    this.boardPoints = [...this.pointsModel.getPoints()];
+
     render(new SortView(), this.boardContainer);
     render(this.eventListComponent, this.boardContainer);
 
     render(new EditPointView(), this.eventListComponent.getElement());
 
-    for (let i = 0; i < POINTS_COUNT; i++) {
+    for (let i = 0; i < this.boardPoints.length; i++) {
       render(new PointView(), this.eventListComponent.getElement());
     }
   }
